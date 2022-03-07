44bec6b42566f02822035f5cc561889c0756eb15
ViralJoe <viralmenias@hotmail.com>
2022-03-06 17:19:15 -0800

    update ready

--- inc/shared/form.inc.php
+++ inc/shared/form.inc.php
@@ -8,6 +8,8 @@ http://php.net/manual/en/language.operators.comparison.php#language.operators.co
 -->
 
 <?php
+// var_dump($_POST);
+// var_dump($student);
 // Button label logic
 if (basename($_SERVER['PHP_SELF']) == 'create-record.php') {
     $button_label = "Save New Record";
@@ -17,7 +19,6 @@ if (basename($_SERVER['PHP_SELF']) == 'create-record.php') {
     $button_label = "Search...";
 }
 ?>
-
 <form class="form-check" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
     <label class="col-form-label" for="first">First Name</label>
     <input class="form-control" type="text" id="first" name="first" value="<?= isset($first) ? $first : null ?>">
@@ -38,31 +39,35 @@ if (basename($_SERVER['PHP_SELF']) == 'create-record.php') {
     <input class="form-control" type="number" id="gpa" name="gpa" min="0" max="4" step="0.01" value="<?= isset($gpa) ? $gpa : "0" ?>">
     <br>
     <div class="form-check">
-        <label class="col-form-label" for="finAid">Do They Recieve Financial Aid</label>
+        <label class="col-form-label">Do They Recieve Financial Aid</label>
         <br>
         <?php
-        // making the radio sticky
-        $finAidYes = false;
-        $finAidNo = true;
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (isset($_POST["financial_aid"])) {
-                if ($_POST["financial_aid"] == "1") {
-                    $finAid = $_POST["financial_aid"];
+            if (isset($_POST["finAid"])) {
+                if ($_POST["finAid"] == "1") {
+                    $finAid = $_POST["finAid"];
+                    $financial_aid = $_POST["finAid"];
                     $finAidYes = true;
                     $finAidNo = false;
-                } else {
-                    $finAid = $_POST["financial_aid"];
+                } elseif ($_POST["finAid"] == "0") {
+                    $finAid = $_POST["finAid"];
+                    $financial_aid = $_POST["finAid"];
                     $finAidYes = false;
                     $finAidNo = true;
                 }
+            }
+        }
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            if (isset($finAidYes)) {
             } else {
-                $program = "select";
+                $finAidYes = false;
+                $finAidNo = true;
             }
         }
         ?>
-        <input class="form-check-input" type="radio" id="finAidYes" name="finAid" value="1" <?= $finAidYes ? "checked" : null ?>>
+        <input class="form-check-input" type="radio" id="finAidYes" name="finAid" value="1" <?= $finAidYes ? 'checked' : null ?>>
         <label class="form-check-label" for="finAidYes">Yes</label><br>
-        <input class="form-check-input" type="radio" id="finAidNo" name="finAid" value="0" <?= $finAidNo ? "checked" : null ?>>
+        <input class="form-check-input" type="radio" id="finAidNo" name="finAid" value="0" <?= $finAidNo ? 'checked' : null ?>>
         <label class="form-check-label" for="finAidNo">No</label>
         <br> <br>
     </div>
@@ -70,20 +75,25 @@ if (basename($_SERVER['PHP_SELF']) == 'create-record.php') {
     // making the dropdown sticky
     if (isset($_POST["program"])) {
         $program = $_POST["program"];
-    } else {
-        $program = "select";
+    }
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        if (isset($program)) {
+        } else {
+            $program = "Undeclared";
+        }
     }
     ?>
     <label class="col-form-label" for="program">Degree Program</label>
-    <select class="form-select" for="program" name="program" id="program">
-        <option value="Undeclared" <?php if ($program == "Undeclared") echo 'selected="selected"'; ?>>Undeclared</option>
-        <option value="AAT Web Development" <?php if ($program == "AAT Web Development") echo 'selected="selected"'; ?>>AAT Web Development</option>
-        <option value="AAT Digital Media Arts" <?php if ($program == "AAT Digital Media Arts") echo 'selected="selected"'; ?>>AAT Digital Media Arts</option>
-        <option value="BAS Cybersecurity" <?php if ($program == "BAS Cybersecurity") echo 'selected="selected"'; ?>>BAS Cybersecurity</option>
-        <option value="AA Mathematics" <?php if ($program == "AA Mathematics") echo 'selected="selected"'; ?>>AA Mathematics</option>
-        <option value="CA Microsoft Technician" <?php if ($program == "CA Microsoft Technician") echo 'selected="selected"'; ?>>CA Microsoft Technician</option>
+    <select class="form-select" name="program" id="program">
+        <option value="Undeclared" <?php if ($program == "Undeclared") echo 'selected'; ?>>Undeclared</option>
+        <option value="AAT Web Development" <?php if ($program == "AAT Web Development") echo 'selected'; ?>>AAT Web Development</option>
+        <option value="AAT Digital Media Arts" <?php if ($program == "AAT Digital Media Arts") echo 'selected'; ?>>AAT Digital Media Arts</option>
+        <option value="BAS Cybersecurity" <?php if ($program == "BAS Cybersecurity") echo 'selected'; ?>>BAS Cybersecurity</option>
+        <option value="AA Mathematics" <?php if ($program == "AA Mathematics") echo 'selected'; ?>>AA Mathematics</option>
+        <option value="CA Microsoft Technician" <?php if ($program == "CA Microsoft Technician") echo 'selected'; ?>>CA Microsoft Technician</option>
     </select>
     <br> <br>
     <a href="display-records.php">Cancel</a>&nbsp;&nbsp;
     <button class="btn btn-primary" type="submit"><?= $button_label ?></button>
+    <input type="hidden" name="id" value="<?= isset($id) ? $id : null ?>">
 </form>
--- inc/update/update.inc.php
+++ inc/update/update.inc.php
@@ -1,4 +1,85 @@
-<?php // Filename: connect.inc.php
+<?php // Filename: update.inc.php
 
 require_once __DIR__ . "/../db/db_connect.inc.php";
 require_once __DIR__ . "/../app/config.inc.php";
+
+$error_bucket = [];
+
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST["id"];
+
+    if (empty($_POST["first"])) {
+        array_push($error_bucket, "<p>A <strong>First Name</strong> is required.</p>");
+    } else {
+        $first = $_POST["first"];
+    }
+    if (empty($_POST["last"])) {
+        array_push($error_bucket, "<p>A <strong>Last Name</strong> is required.</p>");
+    } else {
+        $last = $_POST["last"];
+    }
+    if (empty($_POST["student_id"])) {
+        array_push($error_bucket, "<p>A <strong>Student ID</strong> is required.</p>");
+    } else {
+        $student_id = intval($_POST["student_id"]);
+    }
+    if (empty($_POST["email"])) {
+        array_push($error_bucket, "<p>An <strong>Email</strong> address is required.</p>");
+    } else {
+        $email = $_POST["email"];
+    }
+    if (empty($_POST["phone"])) {
+        array_push($error_bucket, "<p>A <strong>Phone Number</strong> is required.</p>");
+    } else {
+        $phone = $_POST["phone"];
+    }
+
+    // force gpa to insert actual numbers into database
+    $gpa = floatval($_POST["gpa"]);
+
+    $degree_program = $_POST["program"];
+
+    $financial_aid = $_POST["finAid"];
+
+    // If we have no errors than we can try and insert the data
+    if (count($error_bucket) == 0) {
+        $sql = "UPDATE student_v2 SET first_name = :first_name, last_name = :last_name, email = :email, phone = :phone, student_id = :student_id, gpa = :gpa, financial_aid = :financial_aid, degree_program = :degree_program WHERE id = :id";
+        $stmt = $db->prepare($sql);
+        $stmt->execute(["first_name" => $first, "last_name" => $last, "email" => $email, "phone" => $phone, "student_id" => $student_id, "gpa" => $gpa, "financial_aid" => $financial_aid, "degree_program" => $degree_program, "id" => $id]);
+
+        if ($stmt->rowCount() == 0) {
+            echo '<div class="alert alert-danger" role="alert">
+            I am sorry, but I could not update that record for you.</div>';
+        } else {
+            header("Location: display-records.php?message=The record for $first has been updated.");
+        }
+    } else {
+        display_error_bucket($error_bucket);
+    }
+}
+
+$sql = "SELECT * FROM student_v2 WHERE id=:id ";
+$stmt = $db->prepare($sql);
+$stmt->execute(["id" => $id]);
+$student = $stmt->fetch();
+// var_dump($student);
+
+$first = $student->first_name;
+$last = $student->last_name;
+$student_id = $student->student_id;
+$email = $student->email;
+$phone = $student->phone;
+$gpa = $student->gpa;
+$finAid = $student->financial_aid;
+if ($finAid == "1") {
+    $finAidYes = True;
+    $finAidNo = False;
+} else {
+    $finAidYes = False;
+    $finAidNo = True;
+}
+$program = $student->degree_program;
